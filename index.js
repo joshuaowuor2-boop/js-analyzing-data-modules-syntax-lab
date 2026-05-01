003689c61df26c4d5e214a565f6b0b832b20a36f
joshuaowuor2-boop <joshua.owuor2@student.moringaschool.com>
2026-05-01 11:37:23 +0300

    minor fixes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+require('datejs');
 
-
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
